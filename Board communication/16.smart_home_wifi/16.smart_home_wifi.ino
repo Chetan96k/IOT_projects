b76afef7a80bfbcd990ae6ac4ed67d7a971f3ddb
Chetan Kanti <86044384+Chetan96k@users.noreply.github.com>
2023-11-16 16:29:46 +0530

    added comment line

--- Board communication/16.smart_home_wifi/16.smart_home_wifi.ino
+++ Board communication/16.smart_home_wifi/16.smart_home_wifi.ino
@@ -1,3 +1,5 @@
+//smart home wifi
+
 #include <WiFi.h>
 
 // Replace with your network credentials
@@ -6,7 +8,6 @@ const char* password = "password";
 
 // Set web server port number to 80
 WiFiServer server(80);   
-
 // Variable to store the HTTP request
 String header;
 
